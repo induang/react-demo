a66589e27a820b3c993c7439744fa4aeb29a4415
Dwan <yingduan_ge@epam.com>
2023-03-10 18:26:12 +0800

    notication

--- src/utils/noti.ts
+++ src/utils/noti.ts
@@ -1,4 +1,4 @@
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify'
 export default function noti({ type, message }) {
   const t = (toast[type] || toast.info).bind(toast)
   t(message)
