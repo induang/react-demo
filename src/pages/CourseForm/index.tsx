52f2c8491b5d153cd48800b8c460b4ce5ea081c7
Dwan <yingduan_ge@epam.com>
2023-03-17 16:24:29 +0800

    fix-author

--- src/pages/CourseForm/index.tsx
+++ src/pages/CourseForm/index.tsx
@@ -1,5 +1,4 @@
 import { withFormik } from "formik";
-import { useNavigate } from "react-router-dom";
 import { fetchAddCourse, fetchUpdateCourse } from "../../services/course";
 
 import noti from "../../utils/noti";
@@ -29,23 +28,22 @@ export default withFormik<ICourseFormProps, ICourseFormDetail>({
   enableReinitialize: true,
   validationSchema: validator,
   handleSubmit: async (values) => {
-    const navigate = useNavigate();
     let newCourse = {
       title: values.title,
       description: values.description,
-      duration: values.duration,
+      duration: Number(values.duration),
       authors: values.authors,
     };
     if (values.id) {
       console.log("save course");
       fetchUpdateCourse({ ...newCourse, id: values.id }).then(() => {
+        window.location.href = "/courses";
         noti({ type: "success", message: "Update Course Succeed." });
-        navigate("/courses");
       });
     } else {
       fetchAddCourse(newCourse).then(() => {
+        window.location.href = "/courses";
         noti({ type: "success", message: "Add Course Succeed." });
-        navigate("/courses");
       });
     }
   },
